b6c02bdc6d61bc84f2b91835895655fb26fc2cfe
makeetoy <makeetoy0226@gmail.com>
2018-02-05 00:47:32 +0800

    can access the database_tbl
    Updates: 
    * If there's nothing there and you submit, a pop up will appear saying please fill out
    * Name and Email are now successfully stored in the database
    Needed & Fix:
    * Once the submit succeeded it should go back to homepage, it does but the homepage is all jack up.

--- application/views/homepage/homepage_view.php
+++ application/views/homepage/homepage_view.php
@@ -121,11 +121,11 @@
             <form method="post" action="<?php echo base_url();?>Newsletter_controller/submit">
             <div class="row">
             <div class="input-field col s12">
-              <input type="text" class="validate" required>
+              <input type="text" class="validate" name="name" id="name" required>
               <label for="name">Name</label>
             </div>
             <div class="input-field col s12">
-              <input type="text" class="validate" required>
+              <input type="text" class="validate" name="email" id="email" required>
               <label for="email_address">Email Address</label>
             </div>
 
