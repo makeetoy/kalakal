c546089b59b3bf1daba2748076032a05f82df6a8
biadelacruz <biadc22@gmail.com>
2018-02-16 01:18:19 +0800

    added some changes in globalstore_view

--- application/views/store/globalstore_view.php
+++ application/views/store/globalstore_view.php
@@ -3,18 +3,110 @@
 </head>
 <body>
 
+<!--first navigation -->
 <nav class="orange" role="navigation">
   <div class="nav-wrapper container">
-     <a class="left col s3 brand-logo" id="logo-container" href="<?php echo base_url()?>">Kalakal.ph</a>
-     <div class="center col s4 orange lighten-3 nav-wrapper container" style="width:500px;">
-       <form>
-         <div class="input-field">
-           <input id="search" type="search">
-         </div>
-       </form>
-     </div>
+		<a class="center col s3 brand-logo" id="logo-container" href="<?php echo base_url()?>">Kalakal.ph</a>
+		<a data-activates="slide-out" class="button-collapse show-on- " href="#!">
+			<ul class="right hide-on-med-and-down ">	
+				 <li><a href="<?php echo base_url()?>login">Login</a></li>
+				 <li><a href="<?php echo base_url()?>register">Register</a></li>
+			</ul>
 
+<!-- Navbar converts to side menu when mobile mode -->
+          <ul id="nav-mobile" class="side-nav">
+            <li><a href="<?php echo base_url()?>login">Login</a></li>
+            <li><a href="<?php echo base_url()?>register">Register</a></li>
+			<li><a href="<?php echo base_url()?>">View Cart</a></li>
+          </ul>
+          <a href="#" data-activates="nav-mobile" class="button-collapse"><i class="black-text material-icons">menu</i></a>
+    </div>
 </nav>
-<div class="container">
-  <p>Global Store</p>
-</div>
+
+
+<!-- second navigation-->
+<nav class="orange  lighten-3" role="navigation">
+<ul class="left"><a class=" hide-on-med-and-down dropdown-button black-text waves-effect orange waves-light btn" href='' data-activates="user_dropdown " >Categories</a></ul>	 
+
+<!-- Dropdown -->
+  <ul class="dropdown-content" id="user_dropdown">
+    <li><a class="orange-text" href="#!">Category 1</a></li>
+    <li><a class="orange-text" href="#!">Category 2</a></li>
+    <li><a class="orange-text" href="#!">Category 3</a></li>
+	<li><a class="orange-text" href="#!">Category 4</a></li>
+    <li><a class="orange-text" href="#!">Category 5</a></li>
+    <li><a class="orange-text" href="#!">Category 6</a></li>
+  </ul>
+  <ul class="right"><a href="<?php echo base_url()?>"  class="hide-on-med-and-down waves-effect orange waves-light btn black-text">View Cart</a></ul>	
+	
+  <div class="nav-wrapper container">
+		<div class="center col s4 orange lighten-5 nav-wrapper container" >
+				<form>
+					<div class="input-field">
+						<input id="search" type="search">
+					</div>
+				</form>  
+		</div>				
+   </div>
+</nav>
+<br>
+	
+<!--Featured-->
+<div class="container "> 
+<h5>FEATURED</h5>  
+			<ul><div class="center col-sm-2">
+				<a href="#"><img src="https://placehold.it/150x80?text=IMAGE" class="img-responsive" style="height:100px;width:130px" alt="Image"></a>
+				<a href="#"><img src="https://placehold.it/150x80?text=IMAGE" class="img-responsive" style="height:100px;width:130px" alt="Image"></a>
+				<a href="#"><img src="https://placehold.it/150x80?text=IMAGE" class="img-responsive" style="height:100px;width:130px" alt="Image"></a> 
+				<a href="#"><img src="https://placehold.it/150x80?text=IMAGE" class="img-responsive" style="height:100px;width:130px" alt="Image"></a>
+				<a href="#"><img src="https://placehold.it/150x80?text=IMAGE" class="img-responsive" style="height:100px;width:130px" alt="Image"></a>
+				<a href="#"><img src="https://placehold.it/150x80?text=IMAGE" class="img-responsive" style="height:100px;width:130px" alt="Image"></a> 
+				<a href="#"><img src="https://placehold.it/150x80?text=IMAGE" class="img-responsive" style="height:100px;width:130px" alt="Image"></a>
+				<a href="#"><img src="https://placehold.it/150x80?text=IMAGE" class="img-responsive" style="height:100px;width:130px" alt="Image"></a>
+				<a href="#"><img src="https://placehold.it/150x80?text=IMAGE" class="img-responsive" style="height:100px;width:130px" alt="Image"></a>
+				<a href="#"><img src="https://placehold.it/150x80?text=IMAGE" class="img-responsive" style="height:100px;width:130px" alt="Image"></a>
+				
+				</div>
+			
+				
+</ul></div><br>
+
+<!--Top Vendors-->
+<div class="center container "> 
+<h5>TOP VENDORS</h5>  
+			<ul><div class="col-sm-2">
+				<a href="#"><img src="https://placehold.it/150x80?text=IMAGE" class="img-responsive" style="height:100px;width:120px" alt="Image"></a>
+				<a href="#"><img src="https://placehold.it/150x80?text=IMAGE" class="img-responsive" style="height:100px;width:120px" alt="Image"></a>
+				<a href="#"><img src="https://placehold.it/150x80?text=IMAGE" class="img-responsive" style="height:100px;width:120px" alt="Image"></a> 
+				<a href="#"><img src="https://placehold.it/150x80?text=IMAGE" class="img-responsive" style="height:100px;width:120px" alt="Image"></a>
+				<a href="#"><img src="https://placehold.it/150x80?text=IMAGE" class="img-responsive" style="height:100px;width:120px" alt="Image"></a>
+				<a href="#"><img src="https://placehold.it/150x80?text=IMAGE" class="img-responsive" style="height:100px;width:120px" alt="Image"></a>
+			</div>
+</ul></div><br>
+
+<!--Categories-->
+
+
+<ul><div class="center col-sm-2">
+			<h5>CATEGORIES</h5> 
+				<a href="#"><img src="https://placehold.it/150x80?text=IMAGE" class="img-responsive" style="height:120px;width:180px" alt="Image"></a>
+				<a href="#"><img src="https://placehold.it/150x80?text=IMAGE" class="img-responsive" style="height:120px;width:180px" alt="Image"></a>
+				<a href="#"><img src="https://placehold.it/150x80?text=IMAGE" class="img-responsive" style="height:120px;width:180px" alt="Image"></a> 
+				<a href="#"><img src="https://placehold.it/150x80?text=IMAGE" class="img-responsive" style="height:120px;width:180px" alt="Image"></a>
+				<a href="#"><img src="https://placehold.it/150x80?text=IMAGE" class="img-responsive" style="height:120px;width:180px" alt="Image"></a>
+				<a href="#"><img src="https://placehold.it/150x80?text=IMAGE" class="img-responsive" style="height:120px;width:180px" alt="Image"></a>
+				<a href="#"><img src="https://placehold.it/150x80?text=IMAGE" class="img-responsive" style="height:120px;width:180px" alt="Image"></a>
+				<a href="#"><img src="https://placehold.it/150x80?text=IMAGE" class="img-responsive" style="height:120px;width:180px" alt="Image"></a>
+				<a href="#"><img src="https://placehold.it/150x80?text=IMAGE" class="img-responsive" style="height:120px;width:180px" alt="Image"></a> 
+				<a href="#"><img src="https://placehold.it/150x80?text=IMAGE" class="img-responsive" style="height:120px;width:180px" alt="Image"></a>
+				<a href="#"><img src="https://placehold.it/150x80?text=IMAGE" class="img-responsive" style="height:120px;width:180px" alt="Image"></a>
+				<a href="#"><img src="https://placehold.it/150x80?text=IMAGE" class="img-responsive" style="height:120px;width:180px" alt="Image"></a>
+				<a href="#"><img src="https://placehold.it/150x80?text=IMAGE" class="img-responsive" style="height:120px;width:180px" alt="Image"></a> 
+				<a href="#"><img src="https://placehold.it/150x80?text=IMAGE" class="img-responsive" style="height:120px;width:180px" alt="Image"></a>
+				</div>
+</ul>
+
+
+</body>
+</html>
+
