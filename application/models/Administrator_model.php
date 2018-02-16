73ff3590b6606a596cd81ec0213d1f4eaba19f7f
makeetoy <makeetoy0226@gmail.com>
2018-02-17 05:41:09 +0800

    working admin controller

--- application/models/Administrator_model.php
+++ application/models/Administrator_model.php
@@ -1,9 +1,9 @@
 <?php
-class Login_model extends CI_Model
+class Administrator_model extends CI_Model
 {
-  function customerlogin($username,$password){
-    $this->db->where('custUsername',$username);
-    $this->db->where('custPassword',$password);
+  function adminLogin($email,$password){
+    $this->db->where('email',$email);
+    $this->db->where('password',$password);
     $query = $this->db->get('administrator_tbl');
     if($query->num_rows() > 0){
       return true;
