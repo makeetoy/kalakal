8a94c516177bab32d9e197a9e4dfee3c483621f9
LAPTOP-4ONSEAPL\Nicail Pepito <nicail180@gmail.com>
2018-02-17 11:09:33 +0800

    Register Updates

--- application/views/template/footer.php
+++ application/views/template/footer.php
@@ -28,6 +28,8 @@
     </footer>
 
   <!-- Bootstrap core JavaScript -->
+
+  <script src="<?php echo base_url('assets/js/sweetalert.js');?>"></script>
    <script src="<?php echo base_url('assets/js/jquery.min.js');?>"></script>
    <script src="<?php echo base_url('assets/js/bootstrap.bundle.min.js');?>"></script>
 
