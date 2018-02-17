76bdc4b0ec64be65505462827e2470eca8d3614a
kyleaquino <kshaunaquino@gmail.com>
2018-02-17 11:03:00 +0800

    update

--- application/controllers/Customer_controller.php
+++ application/controllers/Customer_controller.php
@@ -4,9 +4,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Customer_controller extends CI_Controller {
 
 	public function index(){
-		$this->load->view('template/header');
-		$this->load->view('accounts/customer/cDashboard_view');
-		$this->load->view('template/footer');
 	}
 
 	public function icon(){
